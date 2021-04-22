23601cc25a36607280e721e5f1080a5e3149e147
Leandro Amorim <leandroamorimpb@gmail.com>
2021-04-22 16:05:41 -0300

    Passando parâmetros para o serviço RESTfullBaixar

--- src/main/java/br/com/cursoapirestsb/controller/IndexController.java
+++ src/main/java/br/com/cursoapirestsb/controller/IndexController.java
@@ -4,15 +4,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
+
+@RestController/*Arquitetura Restfull*/
 @RequestMapping(value = "/usuario")/*isso aparece depois da porta 8080*/
 public class IndexController {
-
+	
+	/*Serviço Restfull
 	@RequestMapping(method = RequestMethod.GET, value = "/", produces = "application/json")
 	public ResponseEntity init() {
 		
 		return new ResponseEntity("Olá Rest Spring boot!!!!", HttpStatus.OK);
+	}
+	*/
+	
+	/*Serviço Restfull - Passando parâmetros*/
+	@RequestMapping(method = RequestMethod.GET, value = "/", produces = "application/json")
+	public ResponseEntity init(@RequestParam (value = "nome", required = true, defaultValue = "Nome não infomardo!") String nome,
+								@RequestParam (value = "cpf", required = true, defaultValue = "000.000.000-00") String cpf) {
+		
+		System.out.println("Parâmetro sendo recebido = " + nome);
+		return new ResponseEntity("Olá Rest Spring boot!!!!\n Seu nome é: " + nome + " CPF: " + cpf, HttpStatus.OK);
 	}
 }
