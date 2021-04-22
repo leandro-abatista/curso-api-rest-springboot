37d4defb510de848216bfb92d0c147b376ec8e82
Leandro Amorim <leandroamorimpb@gmail.com>
2021-04-22 16:19:31 -0300

    Retornando usuário em JSON para a tela

--- src/main/java/br/com/cursoapirestsb/controller/IndexController.java
+++ src/main/java/br/com/cursoapirestsb/controller/IndexController.java
@@ -1,5 +1,8 @@
 package br.com.cursoapirestsb.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
+
+import br.com.cursoapirestsb.model.Usuario;
 
 @RestController/*Arquitetura Restfull*/
 @RequestMapping(value = "/usuario")/*isso aparece depois da porta 8080*/
@@ -21,12 +26,34 @@ public class IndexController {
 	}
 	*/
 	
-	/*Serviço Restfull - Passando parâmetros*/
+	/*Serviço Restfull - Passando parâmetros
 	@RequestMapping(method = RequestMethod.GET, value = "/", produces = "application/json")
 	public ResponseEntity init(@RequestParam (value = "nome", required = true, defaultValue = "Nome não infomardo!") String nome,
 								@RequestParam (value = "cpf", required = true, defaultValue = "000.000.000-00") String cpf) {
 		
 		System.out.println("Parâmetro sendo recebido = " + nome);
 		return new ResponseEntity("Olá Rest Spring boot!!!!\n Seu nome é: " + nome + " CPF: " + cpf, HttpStatus.OK);
+	}
+	*/
+	
+	/*Serviço Restfull - Passando parâmetros*/
+	@RequestMapping(method = RequestMethod.GET, value = "/", produces = "application/json")
+	public ResponseEntity init() {
+		
+		Usuario usuario = new Usuario();
+		usuario.setCodigo(1L);
+		usuario.setLogin("leandro@gmail.com");
+		usuario.setSenha("123");
+		
+		Usuario usuario1 = new Usuario();
+		usuario1.setCodigo(2L);
+		usuario1.setLogin("jose@gmail.com");
+		usuario1.setSenha("456");
+		
+		List<Usuario> usuarios = new ArrayList<>();
+		usuarios.add(usuario);
+		usuarios.add(usuario1);
+		
+		return new ResponseEntity(usuarios, HttpStatus.OK);
 	}
 }
