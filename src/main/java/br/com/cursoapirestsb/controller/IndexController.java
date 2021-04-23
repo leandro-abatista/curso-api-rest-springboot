471dda2497df4095e5f57f27de8935450efc0533
Leandro Amorim <leandroamorimpb@gmail.com>
2021-04-23 09:35:32 -0300

    Criando os métodos customizados HTTP POST, PUT e DELETE

--- src/main/java/br/com/cursoapirestsb/controller/IndexController.java
+++ src/main/java/br/com/cursoapirestsb/controller/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -91,6 +92,10 @@ public class IndexController {
 		return new ResponseEntity(usuario.get(), HttpStatus.OK);
 	}
 	
+	/**
+	 * Lista todos os usuários
+	 * @return
+	 */
 	@RequestMapping(method = RequestMethod.GET, value = "/", produces = "application/json")
 	public ResponseEntity<List<Usuario>> listaUsers() {
 		
@@ -98,6 +103,85 @@ public class IndexController {
 		
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 	}
+	
+	/**
+	 * Cadastrando um usuário
+	 * @param usuario
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.POST, value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+		
+		Usuario usuarioCadastrar = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioCadastrar, HttpStatus.OK);
+	}
+	
+	/**
+	 * Atualizando um usuário
+	 * @param codigo
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.PUT, value = "/atualizar/{codigo}", produces = "application/json")
+	public ResponseEntity<Usuario> atualizarUsuario(@PathVariable ("codigo") Long codigo, @RequestBody Usuario usuario){
+		
+		return usuarioRepository.findById(codigo)
+				.map(record -> {
+					record.setLogin(usuario.getLogin());
+					record.setSenha(usuario.getSenha());
+					record.setNome(usuario.getNome());
+					record.setCpf(usuario.getCpf());
+					record.setTelefone(usuario.getTelefone());
+					record.setEmail(usuario.getEmail());
+					
+					Usuario user = usuarioRepository.save(record);
+					return ResponseEntity.ok().body(user);
+				}).orElse(ResponseEntity.notFound().build());
+	}
+	
+	/**
+	 * excluindo um usuário
+	 * @param codigo
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.DELETE, value = "/{codigo}", produces = "application/json")
+	public ResponseEntity excluirUsuario(@PathVariable ("codigo") Long codigo){
+		
+		return usuarioRepository.findById(codigo)
+				.map(record -> {
+					
+					usuarioRepository.deleteById(codigo);
+					
+					return ResponseEntity.ok().build();
+					
+				}).orElse(ResponseEntity.notFound().build());
+		
+	}
+	
+	/**
+	 * excluindo um usuário
+	 * @param codigo
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.DELETE, value = "/excluir/{codigo}", produces = "application/json")
+	public ResponseEntity excluirUser(@PathVariable ("codigo") Long codigo){
+		
+		usuarioRepository.deleteById(codigo);
+		return ResponseEntity.ok().build();
+		
+	}
+	
+	/*se fosse uma venda
+	@RequestMapping(method = RequestMethod.POST, value = "/vendausuario", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrarVenda(@RequestBody Usuario usuario){
+		
+		/*Aqui seria o processo de venda
+		Usuario usuarioCadastrar = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioCadastrar, HttpStatus.OK);
+	}
+	*/
+	
 	
 	
 }
